2ceb1d2278dc880be2430b81d0eb349b48b88515
Athony·xu <1137847433@qq.com>
2019-07-04 16:01:34 +0800

    短信验证码后端逻辑实现,增加redis数据库读写优化,增加防止图片被多次验证逻辑

---
+++ meiduo_29/meiduo_29/apps/verifications/serializers.py
@@ -0,0 +1,39 @@
+from django_redis import get_redis_connection
+from rest_framework import serializers
+class ImageCodeCheckSerializer(serializers.Serializer):
+    '''图片验证序列化器'''
+    image_code_id = serializers.UUIDField()
+    text = serializers.CharField(max_length=4, min_length=4)
+
+    def validate(self, attrs):
+        # 跨字段联合校验 attrs 一个字典 被检验的数据有哪些
+        print("attrs:%s"%attrs)
+        image_code_id = attrs['image_code_id']
+        text = attrs['text']
+
+        # 查询真实图片验证码
+        redis_conn = get_redis_connection('verify_codes')
+        real_image_code_text = redis_conn.get('img_%s' % image_code_id)
+        if not real_image_code_text:
+            raise serializers.ValidationError('图片验证码无效')
+
+        # 删除redis中的图片验证码
+        redis_conn.delete('img%s'%image_code_id)
+
+        # 比较图片验证码
+        real_image_code_text = real_image_code_text.decode()
+        if real_image_code_text.lower() != text.lower():
+            raise serializers.ValidationError('图片验证码错误')
+
+        # 判断是否在60s内
+        # get_serializer 方法创建序列化器的时候会补充context属性
+        # context 属性中会补充三个值 request format view类视图对象
+        # self.context['view']
+        # django 的类视图 对象中,kwargs保存了路径提取出来的参数
+        mobile = self.context['view'].kwargs['mobile']
+        # 从redis中提取手机号发送记录
+        send_flag = redis_conn.get("send_flag_%s" % mobile)
+        if send_flag:
+            raise serializers.ValidationError('请求次数过于频繁')
+
+        return attrs
--- meiduo_29/meiduo_29/apps/verifications/views.py
+++ meiduo_29/meiduo_29/apps/verifications/views.py
@@ -1,11 +1,20 @@
+import random
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from meiduo_29.libs.captcha.captcha import captcha
 from django_redis import get_redis_connection
+import logging
 
 from verifications import constants
+from verifications.serializers import ImageCodeCheckSerializer
+from meiduo_29.utils.yuntongxun.sms import CCP
 
+logger = logging.getLogger('django')
 
 class ImageCodeView(APIView):
     '''
@@ -30,8 +39,66 @@ class ImageCodeView(APIView):
 
         redis_conn = get_redis_connection('verify_code')
         # 设置有效期setex(存到redis数据库里的键名字,数据有效期,数据)
-
         redis_conn.setex("img_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES, text)
-
+        print("图片验证码是:%s"%text)
         # 返回图片
         return HttpResponse(image,content_type="images/jpg")
+
+
+class SMSCodeView(GenericAPIView):
+    '''访问方式： GET /sms_codes/(?P<mobile>1[3-9]\d{9})/?image_code_id=xxx&text=xxx
+
+    请求参数： 路径参数与查询字符串参数
+
+    参数	            类型	        是否必须	说明
+    mobile	        str	        是	    手机号
+    image_code_id	uuid字符串	是	    图片验证码编号
+    text	        str	        是	    用户输入的图片验证码
+    返回数据： JSON
+
+    返回值	类型	    是否必传	说明
+    message	str	    否	    OK，发送成功'''
+    #声明序列化器类
+    serializer_class = ImageCodeCheckSerializer
+
+    def get(self,request,mobile):
+        # 接受参数 校验参数   序列化器验证
+        # request.query_params  rest自带方法 查询字符串里的所有参数
+        serializer = self.get_serializer(data = request.query_params)
+        #设置验证并且抛出异常
+        serializer.is_valid(raise_exception = True)
+        print(request.urls)
+
+        # 生成短信验证码
+        sms_code = '%06d'%random.randint(0,999999)
+
+        # 保存短信验证码 手机发送记录,
+        redis_conn = get_redis_connection('verify_codes')
+        # redis_conn.setex("sms_%s" % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+        # redis_conn.setex("send_flag_%s" % mobile, constants.SEND_SMS_CODE_INTERVAL, 1)
+        # 使用redis管道提升数据库高并发能力
+        pl = redis_conn.pipeline()
+        pl.setex("sms_%s" % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+        pl.setex("send_flag_%s" % mobile, constants.SEND_SMS_CODE_INTERVAL, 1)
+        # 管道存储的命令执行
+        pl.execute()
+
+        # 发送短信
+        try:
+            ccp = CCP()
+            #参数 手机号 [短信验证码,有效期] 短信模版
+            expires = constants.SMS_CODE_REDIS_EXPIRES // 60
+            result = ccp.send_template_sms(mobile,[sms_code,expires],constants.SMS_CODE_TEMP_ID)
+        except Exception as e:
+            logger.error('短信发送[异常][mobile:%s,message:%s]'%(mobile,e))
+            return Response({'message':'failed'},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            if result == 0:
+                logger.info('短信发送[正常][mobile:%s]'%(mobile))
+                return Response({'message': 'OK'})
+
+            else:
+                logger.warning('短信发送[失败][mobile:%s]'%(mobile))
+                return Response({'message': 'failed'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        # 返回
