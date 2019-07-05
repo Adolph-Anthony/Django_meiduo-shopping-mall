2f563ab671d22835d4f677da1892cc46de0534dd
Athony·xu <1137847433@qq.com>
2019-07-05 19:58:54 +0800

    判断账号是否存在

--- front_end_pc/js/register.js
+++ front_end_pc/js/register.js
@@ -56,12 +56,29 @@ var vm = new Vue({
             this.image_code_url = this.host + "/image_codes/" + this.image_code_id + "/";
         },
         check_username: function (){
-            var len = this.username.length;
+              var len = this.username.length;
             if(len<5||len>20) {
                 this.error_name_message = '请输入5-20个字符的用户名';
                 this.error_name = true;
             } else {
                 this.error_name = false;
+            }
+            // 检查重名
+            if (this.error_name == false) {
+                axios.get(this.host + '/usernames/' + this.username + '/count/', {
+                        responseType: 'json'
+                    })
+                    .then(response => {
+                        if (response.data.count > 0) {
+                            this.error_name_message = '用户名已存在';
+                            this.error_name = true;
+                        } else {
+                            this.error_name = false;
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error.response.data);
+                    })
             }
 
         },
@@ -81,12 +98,28 @@ var vm = new Vue({
             }
         },
         check_phone: function (){
-            var re = /^1[345789]\d{9}$/;
+                   var re = /^1[345789]\d{9}$/;
             if(re.test(this.mobile)) {
                 this.error_phone = false;
             } else {
                 this.error_phone_message = '您输入的手机号格式不正确';
                 this.error_phone = true;
+            }
+            if (this.error_phone == false) {
+                axios.get(this.host + '/mobiles/'+ this.mobile + '/count/', {
+                        responseType: 'json'
+                    })
+                    .then(response => {
+                        if (response.data.count > 0) {
+                            this.error_phone_message = '手机号已存在';
+                            this.error_phone = true;
+                        } else {
+                            this.error_phone = false;
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error.response.data);
+                    })
             }
 
         },
--- meiduo_29/meiduo_29/apps/users/views.py
+++ meiduo_29/meiduo_29/apps/users/views.py
@@ -1,4 +1,72 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users.models import User
 
 # Create your views here.
 
+# url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
+class j(APIView):
+    '''请求方式： GET usernames/(?P<username>\w{5,20})/count/
+
+        请求参数： 路径参数
+
+        参数	        类型	    是否必传	说明
+        username	str	    是	    用户名
+        返回数据： JSON
+
+        {
+            "username": "itcast",
+            "count": "1"
+        }
+        '''
+    """
+    用户名数量
+    """
+    def get(self, request, username):
+        """
+        获取指定用户名数量
+        """
+        count = User.objects.filter(username=username).count()
+
+        data = {
+            'username': username,
+            'count': count
+        }
+
+        return Response(data)
+
+# url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
+class MobileCountView(APIView):
+    """
+    后端接口设计：
+    请求方式： GET mobiles/(?P<mobile>1[3-9]\d{9})/count
+
+    请求参数： 路径参数
+
+    参数	    类型	是否必须	说明
+    mobile	str	是	    手机号
+    返回数据： JSON
+
+    {
+        "mobile": "18512345678",
+        "count": 0
+    }
+    返回值	类型	是否必须	说明
+    mobile	str	是	    手机号
+    count	int	是	    数量
+    手机号数量
+    """
+    def get(self, request, mobile):
+        """
+        获取指定手机号数量
+        """
+        count = User.objects.filter(mobile=mobile).count()
+
+        data = {
+            'mobile': mobile,
+            'count': count
+        }
+
+        return Response(data)
