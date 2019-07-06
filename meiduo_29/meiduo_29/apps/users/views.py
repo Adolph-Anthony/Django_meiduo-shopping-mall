c748a4e3aa00c9428549874159e98a9bc79b1ad1
Athony·xu <1137847433@qq.com>
2019-07-06 20:03:20 +0800

    注册后端代码

--- meiduo_29/meiduo_29/apps/users/views.py
+++ meiduo_29/meiduo_29/apps/users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.generics import  CreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -7,7 +8,7 @@ from users.models import User
 # Create your views here.
 
 # url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
-class j(APIView):
+class UsernameCountView(APIView):
     '''请求方式： GET usernames/(?P<username>\w{5,20})/count/
 
         请求参数： 路径参数
@@ -20,6 +21,10 @@ class j(APIView):
             "username": "itcast",
             "count": "1"
         }
+
+        返回值	    类型	    是否必须	说明
+        username	str	    是	    用户名
+        count	    int	    是	    数量
         '''
     """
     用户名数量
@@ -70,3 +75,36 @@ class MobileCountView(APIView):
         }
 
         return Response(data)
+
+
+
+
+
+class UserView(CreateAPIView):
+    '''
+    用户注册
+    传入参数
+    请求方式： POST /users/
+
+    请求参数： JSON 或 表单
+
+    参数名	    类型	是否必须	说明
+    username	str	是	    用户名
+    password	str	是	    密码
+    password2	str	是	    确认密码
+    sms_code	str	是	    短信验证码
+    mobile	    str	是	    手机号
+    allow	    str	是	    是否同意用户协议
+    返回数据： JSON
+
+    {
+        "id": 9,
+        "username": "python8",
+        "mobile": "18512345678",
+    }
+    返回值	    类型	    是否必须	说明
+    id	        int	    是	    用户id
+    username	str	    是	    用户名
+    mobile	    str	    是	    手机号
+    '''
+    serializer_class = CreateUserSerializer
